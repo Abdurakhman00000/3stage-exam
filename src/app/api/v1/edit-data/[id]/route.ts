ec1bb0f22428db5b7f0e1610b3f264cf2d5b2e8d
Abdurakhman <abdibaitovabdurahman@gmail.com>
2024-11-22 00:42:58 +0600

    fix for deploy

--- src/app/api/v1/edit-data/[id]/route.ts
+++ src/app/api/v1/edit-data/[id]/route.ts
@@ -3,31 +3,41 @@ import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export const PATCH = async (request: Request, { params }: { params: { id: string } }) => {
-  const { id } = params;  
-
-  if (!id) {
-    return NextResponse.json({ message: "Id" }, { status: 400 });
-  }
-
+export const PATCH = async (request: Request) => {
   try {
+    const url = new URL(request.url, `http://${request.headers.get("host")}`);
+    const id = url.pathname.split("/").pop();
+
+    if (!id) {
+      return NextResponse.json({ message: "ID is required" }, { status: 400 });
+    }
+
     const data = await request.json();
-    
+
     if (!data.firstName || !data.lastName) {
-      return NextResponse.json({ message: "success" }, { status: 400 });
+      return NextResponse.json(
+        { message: "First name and last name are required" },
+        { status: 400 }
+      );
     }
 
     const updatedUser = await prisma.user.update({
-      where: { id: Number(id) },  
+      where: { id: Number(id) }, 
       data: {
         firstName: data.firstName,
         lastName: data.lastName,
       },
     });
 
-    return NextResponse.json({ message: "successfully", updatedUser }, { status: 200 });
+    return NextResponse.json(
+      { message: "User updated successfully", updatedUser },
+      { status: 200 }
+    );
   } catch (error) {
-    console.error('Error updating user:', error);
-    return NextResponse.json({ error: 'Failed' }, { status: 500 });
+    console.error("Error updating user:", error);
+    return NextResponse.json(
+      { error: "Failed to update user" },
+      { status: 500 }
+    );
   }
 };
